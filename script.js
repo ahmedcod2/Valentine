03b59855bbe9972ba3d7682ee36c136d8424f3e6
ahmedcod2 <ahmedthaer4@gmail.com>
2024-02-11 21:13:20 -0600

    new dev

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('yesBtn').addEventListener('click', function() {
+    window.location.href = 'loveNote.html';
+});
+
+document.getElementById('noBtn').addEventListener('click', function() {
+    alert('No worries, take your time! I love you always!');
+});
